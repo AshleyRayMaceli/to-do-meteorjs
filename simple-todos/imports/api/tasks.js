38cad085ff29819f3955f63e67f50ae1fc83f852
ashleyraymaceli <ashley.maceli@gmail.com>
2016-09-20 22:22:47 -0700

    add functionality for user to make tasks private

--- simple-todos/imports/api/tasks.js
+++ simple-todos/imports/api/tasks.js
@@ -4,6 +4,22 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // runs on the server
+  // only publish public tasks or tasks made by current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [{
+        private: {
+          $ne: true
+        }
+      }, {
+        owner: this.userId
+      }, ],
+    });
+  });
+}
+
 Meteor.methods({
   'tasks.insert' (text) {
     check(text, String);
@@ -23,11 +39,23 @@ Meteor.methods({
   'tasks.remove' (taskId) {
     check(taskId, String);
 
+    const task = Tasks.findOne(taskId);
+      if (task.private && task.owner !== Meteor.userId()) {
+        // If task is private only the owner can delete it
+        throw new Meteor.Error('not-authorized');
+      }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked' (taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
+
+    const task = Tasks.findOne(taskId);
+      if (task.private && task.owner !== Meteor.userId()) {
+        // If the is private only the owner can check it
+        throw new Meteor.Error('not-authorized');
+      }
 
     Tasks.update(taskId, {
       $set: {
@@ -35,4 +63,22 @@ Meteor.methods({
       }
     });
   },
+  'tasks.setPrivate' (taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+  const task = Tasks.findOne(taskId);
+
+  // Make sure only the task owner can make a task private
+  if (task.owner !== Meteor.userId()) {
+    throw new Meteor.Error('not-authorized');
+  }
+
+  Tasks.update(taskId, {
+    $set: {
+      private: setToPrivate
+      }
+    });
+  },
+
 });
